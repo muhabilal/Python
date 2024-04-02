ce0b6582c4c4647d65ff7008ca691f9a20c19ef5
M Bilal <bilaldarmuhammad@gmail.com>
2024-04-02 21:31:03 +0500

    preprocessing

--- 32ReadCSV.py
+++ 32ReadCSV.py
@@ -1,3 +1,24 @@
 import pandas as pd
-data=pd.read_csv('data.csv')
-print(data["HeartDisease"].dtype)
+data = pd.read_csv('data.csv')
+#print(data['Smoking'].dtype)
+
+# Find null data
+null_data = data.isnull().sum()
+#print(null_data)
+
+# Find duplicate rows
+duplicate_rows = data.duplicated()
+#print(duplicate_rows)
+print(data['HeartDisease'])
+#data['HeartDisease'] = data['HeartDisease'].str.strip().replace({'No': 0, 'Yes':1}) 
+replacements = {
+    'HeartDisease': {'No': 0, 'Yes': 1},
+    'Smoking': {'No': 0, 'Yes': 1},
+    'AlcoholDrinking': {'No': 0, 'Yes': 1},
+    'Stroke':{'No': 0, 'Yes': 1},
+    'DiffWalking':{'No': 0, 'Yes': 1},
+    'Sex':{'Female': 0, 'Male': 1},
+}
+data.replace(replacements, inplace=True)
+data['BMI']=data['BMI'].astype(int)
+data.to_csv('preprocessed_data.csv', index=False)
